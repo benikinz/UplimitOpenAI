dd57696e112633e0c1ec6b317c507b318813ed28
benikinz <93633850+benikinz@users.noreply.github.com>
2023-08-18 15:37:13 -0400

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -40,7 +40,13 @@ def main():
     st.sidebar.subheader("Our Favorites")
     selected_podcast = st.sidebar.selectbox("Select Podcast", options=available_podcast_info.keys())
 
-    if selected_podcast:
+    if process_button or selected_podcast:
+        podcast_info = {}
+        if process_button:
+            podcast_info = process_podcast_info(url)
+        else:  
+            podcast_info = available_podcast_info[selected_podcast]
+
 
         podcast_info = available_podcast_info[selected_podcast]
         background_image_url = podcast_info.get('podcast_dalle', {}).get('image_url', 'default_image_url')
@@ -103,65 +109,7 @@ def main():
     process_button = st.sidebar.button("Process Podcast Feed")
     st.sidebar.markdown("**Note**: Podcast processing can take up to 6 mins, please bear with us.")
 
-    if process_button:
-
-        # Call the function to process the URLs and retrieve podcast guest information
-        podcast_info = process_podcast_info(url)
-
-        #podcast_info = available_podcast_info[selected_podcast]
-        background_image_url = podcast_info.get('podcast_dalle', {}).get('image_url', 'default_image_url')
-
-        
-
-
-        # Create a custom header with the background image
-        header_html = f"""
-        <div id="dallepic"style="background-image: url('{background_image_url}'); background-size: cover; background-repeat: no-repeat; padding: 20px; text-align: center; height: 500px; width: 100%; display: flex; align-items: center; justify-content: center;">
-            <span style="color: white; font-size: 60px; font-weight: bold; text-shadow: 2px 2px 4px #000000;">
-                Benny's Podcasts
-            </span>
-        </div>
-        """
     
-        # Use the `st.markdown` method to render the HTML
-        st.markdown(header_html, unsafe_allow_html=True)
-        st.markdown("Background by DALL·E 2")
-        # Right section - Newsletter content
-        st.header(podcast_info['podcast_details']['episode_title'])
-
-        # Display the podcast title
-        st.subheader(selected_podcast)
-        st.write()
-
-        # Display the podcast summary and the cover image in a side-by-side layout
-        col1, col2 = st.columns([7, 3])
-
-        with col1:
-            # Display the podcast episode summary
-            st.subheader("Podcast Episode Summary")
-            st.write(podcast_info['podcast_summary'])
-
-        with col2:
-            st.image(podcast_info['podcast_details']['episode_image'], caption=selected_podcast, width=300, use_column_width=True)
-
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
-
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
-
-        # Display the five key moments
-        st.subheader("Key Moments")
-        key_moments = podcast_info['podcast_highlights']
-        for moment in key_moments.split('\n'):
-            st.markdown(
-                f"<p style='margin-bottom: 5px;'>{moment}</p>", unsafe_allow_html=True)
-
 
 def create_dict_from_json_files(folder_path):
     json_files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
